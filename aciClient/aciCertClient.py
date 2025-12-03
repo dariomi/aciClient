a1ef5f687275707bda06a4e81a23cfcf509d0aa5
Dario Kaelin <kaelin@netcloud.ch>
2025-12-03 22:14:05 +0100

    Release R1.7

--- aciClient/aciCertClient.py
+++ aciClient/aciCertClient.py
@@ -24,48 +24,52 @@ class ACICert:
     # constructor
     # ==============================================================================
     def __init__(self, apicIp, pkPath, certDn):
-        self.__logger.debug(f'Constructor called {apicIp} {pkPath} {certDn}')
+        self.__logger.debug(f"Constructor called {apicIp} {pkPath} {certDn}")
         self.apicIp = apicIp
-        self.baseUrl = 'https://' + self.apicIp + '/api/'
-        self.__logger.debug(f'BaseUrl set to: {self.baseUrl}')
-        self.pkey = crypto.load_privatekey(crypto.FILETYPE_PEM, open(pkPath, 'rb').read())
+        self.baseUrl = "https://" + self.apicIp + "/api/"
+        self.__logger.debug(f"BaseUrl set to: {self.baseUrl}")
+        self.pkey = crypto.load_privatekey(
+            crypto.FILETYPE_PEM, open(pkPath, "rb").read()
+        )
         self.certDn = certDn
 
     # ==============================================================================
     # packCookies
     # ==============================================================================
     def packCookies(self, content) -> {}:
-        sigResult = base64.b64encode(crypto.sign(self.pkey, content, 'sha256')).decode()
+        sigResult = base64.b64encode(crypto.sign(self.pkey, content, "sha256")).decode()
         # print('sigResultType', type(sigResult), sigResult)
-        return {'APIC-Certificate-Fingerprint': 'fingerprint',
-                'APIC-Certificate-Algorithm': 'v1.0',
-                'APIC-Certificate-DN': self.certDn,
-                'APIC-Request-Signature': sigResult}
+        return {
+            "APIC-Certificate-Fingerprint": "fingerprint",
+            "APIC-Certificate-Algorithm": "v1.0",
+            "APIC-Certificate-DN": self.certDn,
+            "APIC-Request-Signature": sigResult,
+        }
 
     # ==============================================================================
     # getJson
     # ==============================================================================
     def getJson(self, uri) -> {}:
         url = self.baseUrl + uri
-        self.__logger.debug(f'Get Json called url: {url}')
-        content = 'GET/api/' + uri
+        self.__logger.debug(f"Get Json called url: {url}")
+        content = "GET/api/" + uri
         cookies = self.packCookies(content)
         r = requests.get(url, cookies=cookies, verify=False)
 
         # Raise Exception if http Error occurred
         r.raise_for_status()
 
-        self.__logger.debug(f'Successful get Data from APIC: {r.json()}')
-        return r.json()['imdata']
+        self.__logger.debug(f"Successful get Data from APIC: {r.json()}")
+        return r.json()["imdata"]
 
     # ==============================================================================
     # postJson
     # ==============================================================================
     def postJson(self, jsonData):
-        url = self.baseUrl + 'mo.json'
-        self.__logger.debug(f'Post Json called data: {jsonData}')
+        url = self.baseUrl + "mo.json"
+        self.__logger.debug(f"Post Json called data: {jsonData}")
         data = json.dumps(jsonData, sort_keys=True)
-        content = 'POST/api/mo.json' + json.dumps(jsonData)
+        content = "POST/api/mo.json" + json.dumps(jsonData)
         cookies = self.packCookies(content)
         r = requests.post(url, data=data, cookies=cookies, verify=False)
 
@@ -73,19 +77,19 @@ class ACICert:
         r.raise_for_status()
 
         if r.status_code == 200:
-            self.__logger.debug(f'Successful Posted Data to APIC: {r.json()}')
+            self.__logger.debug(f"Successful Posted Data to APIC: {r.json()}")
             return r.status_code
         else:
-            self.__logger.error(f'Error during get occured: {r.json()}')
-            return r.status_code, r.json()['imdata'][0]['error']['attributes']['text']
+            self.__logger.error(f"Error during get occured: {r.json()}")
+            return r.status_code, r.json()["imdata"][0]["error"]["attributes"]["text"]
 
     # ==============================================================================
     # deleteMo
     # ==============================================================================
     def deleteMo(self, dn):
-        self.__logger.debug(f'Delete Mo called DN: {dn}')
-        url = self.baseUrl + 'mo/' + dn + '.json'
-        content = 'DELETE/api/mo/' + dn + '.json'
+        self.__logger.debug(f"Delete Mo called DN: {dn}")
+        url = self.baseUrl + "mo/" + dn + ".json"
+        content = "DELETE/api/mo/" + dn + ".json"
         cookies = self.packCookies(content)
         r = requests.delete(url, cookies=cookies, verify=False)
 
